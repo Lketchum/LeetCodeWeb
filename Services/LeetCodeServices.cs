78741105bf6b6db4da239cf6746f18318935b9b3
Lketchum <liukaixuandell@outlook.com>
2023-08-09 20:22:14 +0800

    62_UniquePaths

--- Services/LeetCodeServices.cs
+++ Services/LeetCodeServices.cs
@@ -1442,7 +1442,30 @@ namespace LeetCodeWeb.Services
 
         public int UniquePaths(int m, int n)
         {
-            return 0;
+            //int[,] pathNum = new int[m, n];
+            //pathNum[0, 0] = 1;
+            //for(int i = 0; i < m; i++)
+            //{
+            //    for(int j = 0; j < n; j++)
+            //    {
+            //        if (j == 0 || i == 0)
+            //            pathNum[i, j] = 1;
+            //        else
+            //            pathNum[i, j] = pathNum[i, j - 1] + pathNum[i - 1, j];
+            //    }
+            //}
+            //return pathNum[m - 1, n - 1];
+
+            //内存优化
+            int[] pathNums = new int[n];
+            for(int i = 0; i < n; i++)            
+                pathNums[i] = 1;
+            for(int i = 1; i < m; i++)
+            {
+                for(int j = 1; j < n; j++)                
+                    pathNums[j] += pathNums[j - 1];                
+            }
+            return pathNums[n - 1];
         }
     }
 }
