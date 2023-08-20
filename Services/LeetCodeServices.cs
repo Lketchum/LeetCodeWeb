74e7b864c7c02c1cea0e8a3eaf9eb66c5841de42
Lketchum <liukaixuandell@outlook.com>
2023-08-20 16:25:14 +0800

    901_StockSpanner

--- Services/LeetCodeServices.cs
+++ Services/LeetCodeServices.cs
@@ -1685,5 +1685,23 @@ namespace LeetCodeWeb.Services
             }
             return null;
         }
+
+        private Stack<Tuple<int, int>> stack;
+        int idx;
+        public void StockSpanner()
+        {
+            stack = new Stack<Tuple<int, int>>();
+            stack.Push(new Tuple<int, int>(-1, int.MaxValue));
+            idx = -1;
+        }
+        public int Next(int price)
+        {
+            idx++;
+            while (price >= stack.Peek().Item2)
+                stack.Pop();
+            int ret = idx - stack.Peek().Item1;
+            stack.Push(new Tuple<int, int>(idx, price));
+            return ret;
+        }
     }
 }
