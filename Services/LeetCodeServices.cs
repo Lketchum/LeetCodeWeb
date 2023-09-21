6db8fb606e88995f0e380a1ec6ae7400bbdfa53c
Lketchum <liukaixuandell@outlook.com>
2023-09-21 22:15:21 +0800

    66_PlusOne

--- Services/LeetCodeServices.cs
+++ Services/LeetCodeServices.cs
@@ -2504,7 +2504,18 @@ namespace LeetCodeWeb.Services
 
         public int[] PlusOne(int[] digits)
         {
-            return null;
+            int n = digits.Length;
+            for (int i = n - 1; i >= 0; i--)
+            {
+                digits[i]++;
+                digits[i] = digits[i] % 10;
+                if (digits[i] != 0)
+                    return digits;
+            }
+            digits = new int[n + 1];
+            digits[0] = 1;
+
+            return digits;
         }
     }
 }
