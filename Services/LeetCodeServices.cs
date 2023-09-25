410be955f8eb26e2eef5ba302ad9809be4addb5e
Lketchum <liukaixuandell@outlook.com>
2023-09-25 15:00:48 +0800

    1572_DiagonalSum

--- Services/LeetCodeServices.cs
+++ Services/LeetCodeServices.cs
@@ -2836,7 +2836,14 @@ namespace LeetCodeWeb.Services
 
         public int DiagonalSum(int[][] mat)
         {
-            return 0;
+            int n = mat.Length;
+            int sum = 0;
+            for (int i = 0; i < n; i++)
+                sum += mat[i][i] + mat[i][n - i - 1];
+            if (n % 2 == 0)
+                return sum;
+            else
+                return sum - mat[n / 2][n / 2];
         }
     }
 }
