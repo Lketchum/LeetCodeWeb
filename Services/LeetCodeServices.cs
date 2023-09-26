dba6c41021282934482d73b214c4135556691427
Lketchum <liukaixuandell@outlook.com>
2023-09-26 17:35:15 +0800

    0926_Afternoon_17

--- Services/LeetCodeServices.cs
+++ Services/LeetCodeServices.cs
@@ -2885,6 +2885,16 @@ namespace LeetCodeWeb.Services
 
         public int[][] SetZeroes(int[][] matrix)
         {
+            int m = matrix.Length;
+            int n = matrix[0].Length;
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+
+                }
+            }
+
             return null;
         }
     }
