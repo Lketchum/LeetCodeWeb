1ebcfad8fb31fcd6fe4512648ece69942dfe3252
Lketchum <liukaixuandell@outlook.com>
2023-10-08 09:12:22 +0800

    707_MyLinkedList_Single

--- Services/CustomStructure.cs
+++ Services/CustomStructure.cs
@@ -412,38 +412,61 @@ namespace LeetCodeWeb.Services
             }
         }
 
-        //链表设计
+        //链表设计-基于单向链表
         public class MyLinkedList
         {
+            int size;
+            ListNode head;
 
             public MyLinkedList()
             {
-
+                size = 0;
+                head = new ListNode(0);
             }
 
             public int Get(int index)
             {
-
+                if (index < 0 || index >= size)
+                    return -1;
+                ListNode cur = head;
+                for (int i = 0; i <= index; i++)
+                    cur = cur.next;
+                return cur.val;
             }
 
             public void AddAtHead(int val)
             {
-
+                AddAtIndex(0, val);
             }
 
             public void AddAtTail(int val)
             {
-
+                AddAtIndex(size, val);
             }
 
             public void AddAtIndex(int index, int val)
             {
-
+                if (index > size)
+                    return;
+                index = Math.Max(0, index);
+                size++;
+                ListNode pred = head;
+                for (int i = 0; i < index; i++)
+                    pred = pred.next;
+                ListNode toAdd = new ListNode(val);
+                toAdd.next = pred.next;
+                pred.next = toAdd;
             }
 
             public void DeleteAtIndex(int index)
             {
-
+                if (index < 0 || index >= size)
+                    return;
+                size--;
+                ListNode pred = head;
+                for (int i = 0; i < index; i++)
+                    pred = pred.next;
+                pred.next = pred.next.next;
             }
         }
 
